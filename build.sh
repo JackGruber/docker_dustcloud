b82d05c6648a42ec5fad511dcd59a85305edf155
JackGruber <24863925+JackGruber@users.noreply.github.com>
2018-05-13 15:34:00 +0200

    add label-schema.org

--- build.sh
+++ build.sh
@@ -7,7 +7,10 @@ if [ "$1" = "test" ]; then
   arch="test"
 fi
 
-docker build -t $REPO:$arch .
+docker build -t $REPO:$arch \
+--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+--build-arg VCS_REF=`git rev-parse --short HEAD` \
+--build-arg BRANCH=`git rev-parse --abbrev-ref HEAD` .
 
 if [ "$1" != "test" ]; then
   case $arch in
@@ -15,18 +18,11 @@ if [ "$1" != "test" ]; then
     docker tag $REPO:$arch $REPO:rpi
     docker tag $REPO:$arch $REPO:rpi3
     docker tag $REPO:$arch $REPO:arm
-
-    # for history
-    docker tag $REPO:$arch $REPO'_pi'
-    if [ "$1" = "push" ]; then
-      docker push $REPO'_pi'
-    fi
     ;;
   armv6l)
     docker tag $REPO:$arch $REPO:rpizw
     ;;
   x86_64)
-    docker tag $REPO:$arch $REPO:x64
     docker tag $REPO:$arch $REPO
     ;;
   *)
