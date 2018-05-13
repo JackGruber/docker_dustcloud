41d832690031512667c59c0dad10254308b3e3c4
JackGruber <24863925+JackGruber@users.noreply.github.com>
2018-05-13 17:30:11 +0200

    change to multiarch manifest

--- build.sh
+++ build.sh
@@ -1,38 +1,19 @@
 #!/bin/sh
 REPO="jackgruber/dustcloud"
 
-arch=$( uname -m )
+ARCH=$(dpkg --print-architecture)
 
 if [ "$1" = "test" ]; then
-  arch="test"
+  ARCH="test"
 fi
 
-docker build -t $REPO:$arch \
+docker build -t $REPO:$ARCH \
 --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
 --build-arg VCS_REF=`git rev-parse --short HEAD` \
 --build-arg BRANCH=`git rev-parse --abbrev-ref HEAD` .
 
-if [ "$1" != "test" ]; then
-  case $arch in
-  armv7l)
-    docker tag $REPO:$arch $REPO:rpi
-    docker tag $REPO:$arch $REPO:rpi3
-    docker tag $REPO:$arch $REPO:arm
-    ;;
-  armv6l)
-    docker tag $REPO:$arch $REPO:rpizw
-    ;;
-  x86_64)
-    docker tag $REPO:$arch $REPO
-    ;;
-  *)
-    echo "Unknown arch $( uname -m )"
-    exit 1
-  ;;
-  esac
 
-  if [ "$1" = "push" ]; then
-    docker rmi $REPO:test
-    docker push $REPO
-  fi
+if [ "$1" = "push" ]; then
+  docker rmi $REPO:test
+  docker push $REPO
 fi
